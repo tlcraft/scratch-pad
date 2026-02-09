c9f9a3bba8bd20bc3e3f14d32796b1a0877a57a0
Travis Craft <tlcraft@users.noreply.github.com>
2026-02-08 21:31:10 -0700

    Setup initial flow.

--- typescript/src/fibonacci/driver.ts
+++ typescript/src/fibonacci/driver.ts
@@ -1,3 +1,9 @@
+const fibonacci = (n: number): number[] => {
+    return [1];
+}
+
 if (require.main === module) {
     console.log('Printing the fibonacci sequence');
+    const sequence = fibonacci(5);
+    console.log('Output: ', sequence);
 }
