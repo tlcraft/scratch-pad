476d7235dfadff33118e4fd6e219d7b2a1a117a4
Travis Craft <tlcraft@users.noreply.github.com>
2026-02-09 17:49:29 -0700

    Flesh out logic.

--- typescript/src/fibonacci/driver.ts
+++ typescript/src/fibonacci/driver.ts
@@ -1,9 +1,23 @@
 const fibonacci = (n: number): number[] => {
-    return [1];
+    if (n <= 0) {
+        return [];
+    } else if (n === 1) {
+        return [0];
+    } else if (n === 2) {
+        return [0, 1];
+    }
+
+    const seq: number[] = [0, 1];
+    for (let i = 2; i < n; i++) {
+        seq.push(seq[i - 1]! + seq[i - 2]!);
+    }
+
+    return seq;
 }
 
 if (require.main === module) {
     console.log('Printing the fibonacci sequence');
+
     const sequence = fibonacci(5);
     console.log('Output: ', sequence);
 }
