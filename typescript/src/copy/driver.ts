b41bd85be006745cc7b9f97f852e1708519a58a0
Travis Craft <tlcraft@users.noreply.github.com>
2026-02-25 17:52:12 -0700

    Add deep copy example.

--- typescript/src/copy/driver.ts
+++ typescript/src/copy/driver.ts
@@ -1,3 +1,9 @@
+const deepCopy = (itemToCopy: any) => {
+    const copy = JSON.parse(JSON.stringify(itemToCopy));
+    console.log(`Deep copy of ${JSON.stringify(itemToCopy)} is ${JSON.stringify(copy)}`);
+    return copy;
+}
+
 const shallowCopy = (itemToCopy: any) => {
     const copy = { ...itemToCopy };
     console.log(`Shallow copy of ${JSON.stringify(itemToCopy)} is ${JSON.stringify(copy)}`);
@@ -16,4 +22,14 @@ if (require.main === module) {
     originalObject.hobbies.push("cooking");
     console.log("Modified Original Object:", originalObject);
     console.log("Shallow Copy after modifying original:", copy);
+    // copy is modifed by the change to the original object because it is a shallow copy, and the hobbies array is shared between the original and the copy.
+
+    const deepCopiedObject = deepCopy(originalObject);
+
+    // Modifying the original object to show the effect of deep copy
+    originalObject.hobbies.push("painting");
+    console.log("Modified Original Object:", originalObject);
+    console.log("Shallow Copy after modifying original:", copy);
+    console.log("Deep Copy after modifying original:", deepCopiedObject);
+    // deepCopiedObject is not modified by the change to the original object because it is a deep copy, and the hobbies array is not shared between the original and the deep copy.
 }
