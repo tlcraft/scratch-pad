54b23116fb57de01778d710d01b21bc549402176
Travis Craft <tlcraft@users.noreply.github.com>
2026-03-06 10:29:56 -0700

    Cleaning up examples and duplicate code.

--- typescript/src/copy/driver.ts
+++ typescript/src/copy/driver.ts
@@ -12,13 +12,7 @@ const objectSpreadCopy = (itemToCopy: any) => {
 
 const jsonStringifyCopy = (itemToCopy: any) => {
     const copy = JSON.parse(JSON.stringify(itemToCopy));
-    console.log(`Deep copy of ${JSON.stringify(itemToCopy)} is ${JSON.stringify(copy)}`);
-    return copy;
-};
-
-const spreadCopy = (itemToCopy: any) => {
-    const copy = { ...itemToCopy };
-    console.log(`Shallow copy of ${JSON.stringify(itemToCopy)} is ${JSON.stringify(copy)}`);
+    console.log(`JSON stringify copy of ${JSON.stringify(itemToCopy)} is ${JSON.stringify(copy)}`);
     return copy;
 };
 
@@ -34,10 +28,11 @@ if (require.main === module) {
     const originalObject = { name: "Alice", age: 30, hobbies: ["reading", "hiking"] };
     console.log("Original Object:", originalObject);
 
-    const spreadCopiedObject = spreadCopy(originalObject);
+    const spreadCopiedObject = objectSpreadCopy(originalObject);
 
     // Modifying the original object to show the effect of shallow copy
     originalObject.hobbies.push("cooking");
+    originalObject.name = "Bob";
     console.log("Modified Original Object:", originalObject);
     console.log("Shallow Copy after modifying the original:", spreadCopiedObject);
     // copy is modifed by the change to the original object because it is a shallow copy, and the hobbies array is shared between the original and the copy.
@@ -59,15 +54,6 @@ if (require.main === module) {
     console.log("Modified Original Object:", originalObject);
     console.log("Object.assign Copy after modifying the original:", objectAssignCopiedObject);
     // objectAssignCopiedObject is not modified by the change to the original object because it is a shallow copy, and the hobbies array is shared between the original and the Object.assign copy.
-
-    const objectSpreadCopiedObject = objectSpreadCopy(originalObject);
-
-    // Modifying the original object to show the effect of Object spread copy
-    originalObject.hobbies.push("swimming");
-    originalObject.name = "Bob";
-    console.log("Modified Original Object:", originalObject);
-    console.log("Object spread Copy after modifying the original:", objectSpreadCopiedObject);
-    // objectSpreadCopiedObject is not modified by the change to the original object because it is a shallow copy, and the hobbies array is shared between the original and the Object spread copy.
 
     const structuredCloneCopiedObject = structuredCloneCopy(originalObject);
     
