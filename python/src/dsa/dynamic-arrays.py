07c0e82ff2e6f30b2417c0735d132f2350513d4f
Travis Craft <tlcraft@users.noreply.github.com>
2026-06-17 07:13:48 -0600

    Update logs, push value.

--- python/src/dsa/dynamic-arrays.py
+++ python/src/dsa/dynamic-arrays.py
@@ -12,7 +12,7 @@ class DynamicArray:
 
     def pop(self):
         if self.__count == 0:
-            raise IndexError("Pop from empty array")
+            raise IndexError('Pop from empty array')
         value = self.array[self.__count - 1]
         self.array[self.__count - 1] = None
         self.__count -= 1
@@ -21,13 +21,13 @@ class DynamicArray:
     def get(self, index):
         if 0 <= index < self.__count:
             return self.array[index]
-        raise IndexError("Index out of bounds")
+        raise IndexError('Index out of bounds')
     
     def set(self, index, value):
         if 0 <= index < self.__count:
             self.array[index] = value
         else:
-            raise IndexError("Index out of bounds")
+            raise IndexError('Index out of bounds')
 
     def _resize(self):
         self.__capacity = 2 * self.__capacity if self.__capacity else 1
@@ -39,19 +39,20 @@ class DynamicArray:
     def size(self):
         return self.__count
 
-if __name__ == "__main__":
-    print("Dynamic Arrays in Python")
+if __name__ == '__main__':
+    print('Dynamic Arrays in Python')
 
     dynamic_array = DynamicArray()
     dynamic_array.push(2)
     dynamic_array.push(4)
+    dynamic_array.push(6)
 
     for i in range(2):
         print(dynamic_array.get(i))
 
     dynamic_array.set(0, 10)
-    print(dynamic_array.get(0))
+    print('First value: ', dynamic_array.get(0))
 
-    print("Popped value:", dynamic_array.pop())
+    print('Popped value: ', dynamic_array.pop())
     for i in range(dynamic_array.size()):
         print(dynamic_array.get(i))
