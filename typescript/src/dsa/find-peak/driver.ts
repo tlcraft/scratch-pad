b6f95a689ae8ac21b39e272a9049fab689d1e0a7
Travis Craft <tlcraft@users.noreply.github.com>
2026-06-25 19:19:27 -0600

    Add search.

--- typescript/src/dsa/find-peak/driver.ts
+++ typescript/src/dsa/find-peak/driver.ts
@@ -1,13 +1,25 @@
+// Find the peak of graph given array of points with a single peak. Uses binary search.
 const findPeak = (arr: number[]): number => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (arr[mid]! < arr[mid + 1]!) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
     
-    return 0;
+    return left;
 }
 
 if (require.main === module) {
     console.log('Locating the peak of an array');
 
     const values = [1, 3, 5, 7, 6, 4, 2];
-
     const peakIndex = findPeak(values);
     console.log(`Peak found at index ${peakIndex} with value ${values[peakIndex]}`);
 }
